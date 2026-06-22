4f633347d479583e5a62c3bd3f6b5b19f3951211
Khaoula-Ghalimi <github@khaoula-ghalimi.com>
2026-06-22 22:30:38 +0100

    cleaning the repo to restart again

--- packages/react-native-yolo/ios/Bridge.h
+++
@@ -1,8 +0,0 @@
-//
-//  Bridge.h
-//  yolo
-//
-//  Created by Khaoula-Ghalimi on 17/06/2026
-//
-
-#pragma once
--- packages/react-native-yolo/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { NitroModules } from 'react-native-nitro-modules'
-import type { Yolo as YoloSpec } from './specs/yolo.nitro'
-
-export const Yolo =
-  NitroModules.createHybridObject<YoloSpec>('Yolo')
